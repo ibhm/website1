cf8151ca14f1eff0dd60107e6386c169552c667e
ibhm <maliki2@mcmaster.ca>
2022-06-11 12:56:31 -0400

    new files

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Home msg="Ibrahim Malik.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
@@ -20,7 +19,14 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  padding: 0;
+  margin: 0;
+}
+
+body{
+  background: black;
+  padding: 0;
+  margin: 0;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="hello">
+
+    <div class="menu">
+        <div class = "menu-item">
+          Resume
+      </div>
+        <div class = "menu-item">
+          Photography
+      </div>
+      <div class = "menu-item">
+          About
+      </div>
+    </div>
+
+    <div class ="name">
+      <div class="picture">
+        <img src ="../assets/logogif.gif" class = "image">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home-page',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#app{
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -mo-os-font-smoothing: black;
+  color: white;
+
+  padding:0;
+  margin:0;
+}
+
+body{
+  background: #080808;
+  padding:0;
+  margin:0;
+}
+.name{
+  border: 1px solid black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  vertical-align: middle;
+  width: 100vw;
+  height: fit-content;
+}
+
+.picture{
+  margin: 5 px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  vertical-align: middle;
+}
+
+.image{
+  width:100%;
+}
+
+.menu{
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin-top: 10px;
+  height: 60px;
+}
+
+.menu-item{
+  flex: 1;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+
+.menu-text{
+  font-size: 20px;
+
+}
+
+
+
+
+</style>
